8d93773970edaabb7c7445f952a1b53ecae3b1d0
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2020-01-05 20:55:58 +0100

    Setup data grid for displaying developer count per city

---
+++ src/webpack.config.js
@@ -0,0 +1,40 @@
+//@ts-check
+
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+    mode: 'development',
+    entry: {
+        main: './ts/main.ts'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: './*.html' },
+            { from: './../data/*.json', flatten: true },
+        ]),
+    ]
+};
+
+module.exports = config;
